503a7e31a661b16e8e71d1963495761d5625d20e
Pherola Jordana Axelson dos Santos <112866658+pherola@users.noreply.github.com>
2024-09-03 14:29:11 -0300

    concluindo

--- script.js
+++ script.js
@@ -6,21 +6,21 @@ const textoResultado = document.querySelector(".texto-resultado");
 
 const perguntas = [
     {
-        enunciado: "Assim que saiu da escola você se depara com uma nova tecnologia, um chat que consegue responder todas as dúvidas que uma pessoa pode ter, ele também gera imagens e áudios hiper-realistas. Qual o primeiro pensamento?",
+        enunciado: "Ultimamente, as pessoas estão se importando com sua saúde física e mental, além de se preocuparem muito com sua aparência. Por um lado, isso é bom, pois devemos bucar nossa melhor versão e estar sempre em evolução. Por outro lado, se ficarmos obcecados em nossa aparência, isso pode fazer mal para nossa mente e corpo, então devemos manter equilíbrio. E você, pratica exercício físico ultimamente?",
         alternativas: [
             {
-                texto: "Isso é assustador!",
-                afirmacao: "No início ficou com medo do que essa tecnologia pode fazer. "
+                texto: "Sim",
+                afirmacao: "Isso é muito bom! Quanto exercitamos nosso corpo, também trabalhamos nossa mente",
             },
             {
-                texto: "Isso é maravilhoso!",
-                afirmacao: "Quis saber como usar IA no seu dia a dia."
+                texto: "Não",
+                afirmacao: "Se você se sente bem assim, está ótimo!",
             }
         ]
     },
     {
-        enunciado: "Com a descoberta desta tecnologia, chamada Inteligência Artificial, uma professora de tecnologia da escola decidiu fazer uma sequência de aulas sobre esta tecnologia. No fim de uma aula ela pede que você escreva um trabalho sobre o uso de IA em sala de aula. Qual atitude você toma?",
-        alternativas: [
+        enunciado: "Você conhece a importância do exercício físico? A prática regular de exercícios físicos envolve muito mais do que estética. Ela contribui para o bem-estar físico, saúde mental, bom funcionamento do coração, circulação sanguínea, da respiração e até mesmo dos hormônios. Mesmo sabendo disso, nem sempre é fácil conseguir tempo ou mesmo motivação para manter uma prática regular de exercícios físicos, ao menos 3 vezes na semana. Você prefere esportes ",
+        alternativas:  [
             {
                 texto: "Utiliza uma ferramenta de busca na internet que utiliza IA para que ela ajude a encontrar informações relevantes para o trabalho e explique numa linguagem que facilite o entendimento.",
                 afirmacao: "Conseguiu utilizar a IA para buscar informações úteis."
